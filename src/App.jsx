d1f2221f43d0a4e70719a7833b2936669fa77136
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-07-22 18:24:36 -0300

    Refactor: src/App.jxs

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,34 @@ import Button from "./Components/Button/Button";
 import TitleInput from "./Components/Input/TitleInput";
 import TodosList from "./Components/ItemList/TodoList.jsx";
 import { createDummyData } from "./test/fake/fakeList";
-
-
-const fakeList = createDummyData();
+import { useState } from "react";
 
 function App() {
+  const [fakeList, setFakeList] = useState(createDummyData());
+
+  const addTodo = (toAdd) => {
+    setFakeList((prevState) =>
+      prevState.map((todo) => ({ ...todo })).push(todo)
+    );
+  };
+  const editTodo = (toUpdate) => {
+    setFakeList((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === toUpdate.id) {
+          return { ...toUpdate };
+        }
+        return { ...todo };
+      })
+    );
+  };
+
+  const deleteTodo = (toDelete) => {
+    setFakeList((prevState) =>
+      prevState
+        .filter((todo) => todo.id !== toDelete.id)
+        .map((todo) => ({ ...todo }))
+    );
+  };
   return (
     <div className="App">
       <div className="App-header">
@@ -25,11 +48,15 @@ function App() {
       </div>
 
       <div className="App-todo-list">
-        <TodosList initialList={fakeList} />
+        <TodosList
+          initialList={fakeList}
+          onEditItem={editTodo}
+          onDeleteItem={deleteTodo}
+        />
       </div>
 
       <div className="App-add-button">
-        <Button {...{ attributes: { className: "add-big" } }}>
+        <Button {...{ attributes: { className: "add-big", onClick: addTodo } }}>
           {() => "Agregar"}
         </Button>
       </div>
